a2e267f27e1c886878c7efea33c99543d0d441c2
Salah A.Fattah <salahfattah1@outlook.com>
2022-04-10 16:04:32 +0200

    Create 16*16 webpage with squared divs

---
+++ script.js
@@ -0,0 +1,23 @@
+const bod=document.querySelector("body");
+
+function createDivs(){
+    let container=document.createElement("div");
+    container.classList.add("container");
+    bod.insertBefore(container,document.querySelector("script"));
+    for(let i=0;i<256;i++){
+        let elem=document.createElement("div");
+        elem.classList.add("square");
+        container.appendChild(elem);
+    }
+}
+createDivs();
+
+function square(div){
+    const screenWidth=document.body.clientWidth;
+    //to create 16 boxes according to different screen sizes
+    const squareWidth=screenWidth/16;
+    div.style.width=squareWidth+"px";
+    div.style.height=squareWidth+"px";
+}
+let squares=document.querySelectorAll(".square");
+squares.forEach(i=>square(i));
